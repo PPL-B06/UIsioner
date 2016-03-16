445ac9b64baa8ee85dce4f8f044b1a397644a77e
Ilham Kurniawan <ilhamkurni@gmail.com>
2016-03-16 10:55:24 +0700

    do "composer update" in command line

--- application/vendor/bin/psysh.bat
+++ application/vendor/bin/psysh.bat
@@ -1,4 +1,3 @@
 @ECHO OFF
-setlocal DISABLEDELAYEDEXPANSION
 SET BIN_TARGET=%~dp0/../psy/psysh/bin/psysh
 php "%BIN_TARGET%" %*
--- application/vendor/composer/autoload_namespaces.php
+++ application/vendor/composer/autoload_namespaces.php
@@ -7,6 +7,7 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'phpDocumentor' => array($vendorDir . '/phpdocumentor/reflection-docblock/src'),
+    'SSO' => array($vendorDir . '/ristek/sso'),
     'Psr\\Log\\' => array($vendorDir . '/psr/log'),
     'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
     'Mockery' => array($vendorDir . '/mockery/mockery/library'),

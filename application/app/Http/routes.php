2ec28247dbe604f89c9b56d05139bd1f7c51031f
Ilham Kurniawan <ilhamkurni@gmail.com>
2016-03-16 10:45:33 +0700

    routing for login

--- application/app/Http/routes.php
+++ application/app/Http/routes.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/login', 'UserController@login');
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes

2d9282ce219eec3178d90ff187d2d5e7550d510a
Ilham Kurniawan <ilhamkurni@gmail.com>
2016-03-20 20:41:08 +0700

    reindent and add csrf_token()

--- application/resources/views/register.blade.php
+++ application/resources/views/register.blade.php
@@ -32,53 +32,52 @@
 @section('content')
 <div class="container">
     <div class="row">
-         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                 <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
-                  {!! csrf_field() !!}
-
-
-                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                        <label class="col-md-4 control-label">E-Mail</label>
-                        <div class="col-md-6">
-                            <input type="email" class="form-control" name="email" value="{{ old('email') }}">
-                             @if ($errors->has('email'))
-                                <span class="help-block">
-                                  <strong>{{ $errors->first('email') }}</strong>
-                                </span>
-                              @endif
-
-                        </div>
-                    </div>
-
-                     <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
-                        <label class="col-md-4 control-label">Handphone</label>
-                        <div class="col-md-6">
-                            <input type="text" class="form-control" name="phone" value="{{ old('phone') }}">
-                              @if ($errors->has('phone'))
-                                <span class="help-block">
-                                  <strong>{{ $errors->first('phone') }}</strong>
-                                </span>
-                              @endif
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <div class="col-md-6 col-md-offset-4">
-                            <button type="submit" class="btn btn-primary btn-block right">
-                              <i class="fa fa-btn fa-user"></i>Register
-                            </button>
-                        </div>
-
-                    </div>
-                 </form>
-                </div>
+     <div class="col-md-8 col-md-offset-2">
+      <div class="panel panel-default">
+        <div class="panel-heading">Register</div>
+        <div class="panel-body">
+         <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+          {!! csrf_field() !!} 
+          <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+            <label class="col-md-4 control-label">E-Mail</label>
+            <div class="col-md-6">
+              <input type="email" class="form-control" name="email" value="{{ old('email') }}"> 
+              @if ($errors->has('email')) 
+              <span class="help-block"> 
+                <strong>{{ $errors->first('email') }}</strong> 
+              </span>
+              @endif
             </div>
-        </div>
-     
+          </div>
+
+          <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
+            <label class="col-md-4 control-label">Handphone</label>
+            <div class="col-md-6">
+              <input type="text" class="form-control" name="phone" value="{{ old('phone') }}"> 
+              @if ($errors->has('phone')) 
+              <span class="help-block">
+                <strong>{{ $errors->first('phone') }}</strong>
+              </span> 
+              @endif 
+            </div>
+          </div>
+
+          <div class="form-group">
+            <div class="col-md-6 col-md-offset-4">
+              <button type="submit" class="btn btn-primary btn-block right">
+                <i class="fa fa-btn fa-user"></i>Register
+              </button>
+            </div>
+
+          </div>
+
+          <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+        </form>
+      </div>
     </div>
+  </div>
+  
+</div>
 </div>
 @stop
 

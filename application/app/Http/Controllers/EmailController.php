bdfecd9be33d259adf0bcd11bd501e4111818981
Ilham Kurniawan <ilhamkurni@gmail.com>
2016-05-21 22:26:50 +0700

    improve email blast (using uisioner@gmail.com)

--- application/app/Http/Controllers/EmailController.php
+++ application/app/Http/Controllers/EmailController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Mail;
+use DB;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
@@ -12,33 +13,72 @@ class EmailController extends Controller
 {
     public function send(Request $request){
         if(\SSO\SSO::authenticate()){
-            $values = array(
-                "ilham907"=>"ilham907@gmail.com",
-                "ilhamline" => "ilhamline8@gmail.com",
-                "ilham kurniawan" => "ilhamkurni@gmail.com"
-                );
+            if (session()->get('role')=="admin"){
+                // kode dan nama fakultas yang berada di SSO
+                $codeOrg = array(
+                    "01" => "FK",
+                    "02" => "FKG",
+                    "03" => "MIPA",
+                    "04" => "TEKNIK",
+                    "05" => "HUKUM",
+                    "06" => "FEB",
+                    "07" => "FIB",
+                    "08" => "PSIKOLOGI",
+                    "09" => "FISIP",
+                    "10" => "FKM",
+                    "12" => "FASILKOM",
+                    "13" => "FIK",
+                    "14" => "PASCASARJANA",
+                    "15" => "VOKASI",
+                    "16" => "PEROLEHAN KREDIT",
+                    "17" => "FARMASI"
+                    );
 
-            // lakukan iterasi sebanyak isi dari array values
-            foreach ($values as $name => $email) {
+                // inisiasi array yang berisi kumpulan form yang ingin dilakukan blast email berdasarkan fakultas
+                $emailBlast = [];
+                foreach ($codeOrg as $code => $facultyName) {
+                    // hitung jumlah form yang belum di boost berdasarkan filter yang sesuai
+                    $itemPerFaculty = DB::table('form')->join('filter', 'form.ID', '=', 'filter.form_ID')->where([['filter.type','=','Faculty'],['filter.name','=',$code],['form.boosted','=','0']])->count();
 
-            // kirim email dengan view: /views/emails/welcome.blade.php
-            // variabel yang dibutuhkan:
-            // values = array of email tujuan
-            // name = keys of values array
-            // sub = email subject
-            // web = variable yang dipakai di view
+                    // jika jumlah form > 0 maka masukkan ke dalam array
+                    if ($itemPerFaculty>0) {
+                        $emailBlast[$facultyName]=$itemPerFaculty;
+                    }
+                }
 
-                $sub = "Hi {$name}! Selamat datang di UIsioner";
+                // jika array emailBlast tidak kosong, lakukan kirim email
+                if(!empty($emailBlast)){
+                    $values = array(
+                        "ilham907"=>"ilham907@gmail.com",
+                        "ilhamline" => "ilhamline8@gmail.com",
+                        "ilham kurniawan" => "ilhamkurni@gmail.com"
+                    );
 
-                Mail::send('emails.welcome', ['web' => "UIsioner"], function ($message) use ($name,$values,$sub) {
-                    
-                    $message->from('ilhamkurni@gmail.com', 'Admin UIsioner');
+                    // lakukan iterasi sebanyak isi dari array values
+                    foreach ($values as $name => $email) {
+                        // kirim email dengan view: /views/emails/blast.blade.php
+                        // variabel yang dibutuhkan:
+                        // values = array of email tujuan
+                        // name = keys of values array
+                        // sub = email subject
+                        // web = variable yang dipakai di view
 
-                    $message->to($values[$name])->subject($sub);
+                        $sub = "Hi {$name}! Yuk isi form baru di UIsioner :) (final)";
 
-                });
+                        Mail::send('emails.blast', ['array' => $emailBlast], function ($message) use ($name,$values,$sub) {
+                            $message->from('uisioner@gmail.com', 'Admin UIsioner');
+                            $message->to($values[$name])->subject($sub);
+                        });
+                    }
+                    // ubah variabel boosted menjadi 1, karena email sudah berhasil terkirim
+                    DB::table('form')->update(['form.boosted' => 1]);
+
+                    return "Your email has been sent successfully";
+                }
             }
-            return "Your email has been sent successfully";    
+            else{
+                return view('denied');
+            }
         }
     }
 }
---
+++ application/resources/views/emails/blast.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<h2>Learning Laravel!</h2>
+<br>
+<p>Berikut adalah list kuesioner yang tersedia, silahkan isi dengan akses http://uisioner.com</p>
+<div>
+    
+    @foreach ($array as $key => $value)
+	    @if($value > 0)
+    		tersedia kuesioner dari {{ $key }} : sebanyak {{ $value }} <br>
+	    @endif
+    @endforeach
+</div>
+
+</body>
+</html>

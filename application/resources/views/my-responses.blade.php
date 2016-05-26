8445b3512ff82bfdf85e0db7f66a0715610bb5df
Ilham Kurniawan <ilhamkurni@gmail.com>
2016-05-26 09:46:25 +0700

    few views revision

--- application/resources/views/my-responses.blade.php
+++ application/resources/views/my-responses.blade.php
@@ -16,8 +16,9 @@
       <h4 class="text-uppercase title">My Responses</h4>
       <div class="panel panel-default">
         <div class="panel-body">
+          <div class="row">
             @foreach($resp_forms as $resp_form)
-            <div class="row">
+            
             <div class="col-xs-12">
               <h4>{{ $resp_form->Title }} <br/><small>filled on {{ date('l, F jS Y', strtotime($resp_form->Time_Stamp)) }}</small></h4>
             </div>
--- application/resources/views/view-form.blade.php
+++ application/resources/views/view-form.blade.php
@@ -15,24 +15,25 @@
       <p><a href="#">Link</a></p>-->
     </div>
     <div class="col-sm-8 text-left" style="margin-bottom:20px;"> 
-      <h3>View Form</h3>
-    <hr>
-	<form role="form" method="POST" action="{{ url('/postAnswer') }}">
-  	@foreach ($questions as $question)
-    <label class="control-label" for="title">{{$question->Title}}</label>
-    <input type="text" name="{{ $question->ID }}" class="form-control" pattern="([\32-\x7E]){1,127}$" required disabled>
-    <hr>
+      <h4 class="text-uppercase title">View Form</h4>
+     <div class="panel panel-default">
+      <div class="panel-body">
+      	<form role="form" method="POST" action="{{ url('/postAnswer') }}">
+        	@foreach ($questions as $question)
+          <label class="control-label txtMont" for="title">{{$question->Title}}</label>
+          <input type="text" name="{{ $question->ID }}" class="form-control" pattern="([\32-\x7E]){1,127}$" required disabled>
+          <hr>
 
-	@endforeach
-  <input type="text" name="formID" value="{{ $formID }}" hidden>
-  <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
-       <button type="submit" class="btn btn-default animate pull-right" disabled>Submit</button>
-    </div>
-  </div>
-  <input type="hidden" name="_token" value="{{ csrf_token() }}">
-	</form>
-    
+        	@endforeach
+          <input type="text" name="formID" value="{{ $formID }}" hidden>
+          <div class="form-group">
+              <button type="submit" class="btn btn-default animate pull-right" disabled>Submit</button>
+          </div>
+  
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+	         </form>
+          </div>
+          </div>
     </div>
     <div class="col-sm-2 sidenav">
       <!--<div class="well">
